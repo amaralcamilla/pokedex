6191717e18607a7a4abfc08192e378d50a252dc3
Camilla <61126450+amaralcamilla@users.noreply.github.com>
2021-10-21 18:31:24 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from "react";
-import DATA from './assets/pokemon_data'
-import Pokemon from './components/Pokemon'
+import Home from '../src/pages/Home'
+
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <h1 className="title">Pokedex</h1>
-        <div className="container-pokemons">
-
-        {DATA.map((item) => ( //"Percorra meu array de data e a cada loop compile e renderize cada dado dele"
-            <Pokemon
-              key={item.id} //Props pré-definida. O react já entende que id é key.
-              name={item.name.english}
-              photo={item.image}
-              attack={item.base.Attack}
-              defense={item.base.Defense}
-              hp={item.base.hp}
-              types={item.type}
-              />
-        ))}
-
-        </div>
-      </div>
-    )
+    return <Home />
   }
 }
 
